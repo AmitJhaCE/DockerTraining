fca67b5f7bf35a5f7b6232c6e175bf3a561ec2ae
AmitJhaCE <amitjha@creativeentropy.in>
2022-10-05 02:17:48 +0530

    Change in server file

--- NodeAppDataVolumes/server.js
+++ NodeAppDataVolumes/server.js
@@ -41,6 +41,6 @@ app.post('/create', async (req, res) => {
     }
   });
 });
+console.log('Server running at http://127.0.0.1:80');
 
 app.listen(80);
-console.log('Server running at http://127.0.0.1:80');
